aea8fe0c331d6bd6e70a64fee34930cb7200bdfe
wpop <wpopkov@gmail.com>
2026-06-27 12:59:17 -0700

    Draw OpenGL image texture

--- src/render/OpenGLSliceViewer.cpp
+++ src/render/OpenGLSliceViewer.cpp
@@ -1,5 +1,8 @@
 #include "qtviewerpro/render/OpenGLSliceViewer.h"
 
+#include <QOpenGLContext>
+#include <QOpenGLExtraFunctions>
+
 namespace qvp
 {
 
@@ -9,11 +12,15 @@ OpenGLSliceViewer::OpenGLSliceViewer(QWidget* parent) : QOpenGLWidget(parent)
 
 OpenGLSliceViewer::~OpenGLSliceViewer()
 {
-  if (textureId_ != 0 && context())
+  if (context())
   {
     makeCurrent();
-    glDeleteTextures(1, &textureId_);
-    textureId_ = 0;
+    if (textureId_ != 0)
+    {
+      glDeleteTextures(1, &textureId_);
+      textureId_ = 0;
+    }
+    destroyRenderingResources();
     doneCurrent();
   }
 }
@@ -34,6 +41,7 @@ void OpenGLSliceViewer::initializeGL()
 {
   initializeOpenGLFunctions();
   glGenTextures(1, &textureId_);
+  initializeRenderingResources();
   glClearColor(17.0F / 255.0F, 17.0F / 255.0F, 17.0F / 255.0F, 1.0F);
 }
 
@@ -46,6 +54,157 @@ void OpenGLSliceViewer::paintGL()
 {
   glClear(GL_COLOR_BUFFER_BIT);
   uploadTextureIfNeeded();
+
+  if (!hasImage() || textureId_ == 0 || shaderProgram_ == 0)
+  {
+    return;
+  }
+
+  auto* extraFunctions = QOpenGLContext::currentContext()->extraFunctions();
+  glUseProgram(shaderProgram_);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, textureId_);
+  glUniform1i(glGetUniformLocation(shaderProgram_, "imageTexture"), 0);
+
+  extraFunctions->glBindVertexArray(vao_);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
+  extraFunctions->glBindVertexArray(0);
+
+  glBindTexture(GL_TEXTURE_2D, 0);
+  glUseProgram(0);
+}
+
+void OpenGLSliceViewer::initializeRenderingResources()
+{
+  static constexpr char kVertexShaderSource[] = R"(
+#version 330 core
+layout (location = 0) in vec2 position;
+layout (location = 1) in vec2 texCoord;
+
+out vec2 fragmentTexCoord;
+
+void main()
+{
+  fragmentTexCoord = texCoord;
+  gl_Position = vec4(position, 0.0, 1.0);
+}
+)";
+
+  static constexpr char kFragmentShaderSource[] = R"(
+#version 330 core
+in vec2 fragmentTexCoord;
+
+out vec4 outputColor;
+
+uniform sampler2D imageTexture;
+
+void main()
+{
+  outputColor = texture(imageTexture, fragmentTexCoord);
+}
+)";
+
+  const GLuint vertexShader = compileShader(GL_VERTEX_SHADER, kVertexShaderSource);
+  const GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, kFragmentShaderSource);
+  if (vertexShader == 0 || fragmentShader == 0)
+  {
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return;
+  }
+
+  shaderProgram_ = glCreateProgram();
+  glAttachShader(shaderProgram_, vertexShader);
+  glAttachShader(shaderProgram_, fragmentShader);
+  glLinkProgram(shaderProgram_);
+
+  GLint linkStatus = GL_FALSE;
+  glGetProgramiv(shaderProgram_, GL_LINK_STATUS, &linkStatus);
+
+  glDetachShader(shaderProgram_, vertexShader);
+  glDetachShader(shaderProgram_, fragmentShader);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  if (linkStatus != GL_TRUE)
+  {
+    glDeleteProgram(shaderProgram_);
+    shaderProgram_ = 0;
+    return;
+  }
+
+  static constexpr float kQuadVertices[] = {
+      -1.0F, -1.0F, 0.0F, 1.0F,
+       1.0F, -1.0F, 1.0F, 1.0F,
+       1.0F,  1.0F, 1.0F, 0.0F,
+      -1.0F, -1.0F, 0.0F, 1.0F,
+       1.0F,  1.0F, 1.0F, 0.0F,
+      -1.0F,  1.0F, 0.0F, 0.0F,
+  };
+
+  auto* extraFunctions = QOpenGLContext::currentContext()->extraFunctions();
+  extraFunctions->glGenVertexArrays(1, &vao_);
+  glGenBuffers(1, &vbo_);
+
+  extraFunctions->glBindVertexArray(vao_);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER,
+               static_cast<qopengl_GLsizeiptr>(sizeof(kQuadVertices)),
+               kQuadVertices,
+               GL_STATIC_DRAW);
+
+  constexpr GLsizei kStride = 4 * sizeof(float);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, kStride, nullptr);
+
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1,
+                        2,
+                        GL_FLOAT,
+                        GL_FALSE,
+                        kStride,
+                        reinterpret_cast<const void*>(2 * sizeof(float)));
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  extraFunctions->glBindVertexArray(0);
+}
+
+void OpenGLSliceViewer::destroyRenderingResources()
+{
+  if (vbo_ != 0)
+  {
+    glDeleteBuffers(1, &vbo_);
+    vbo_ = 0;
+  }
+
+  if (vao_ != 0)
+  {
+    QOpenGLContext::currentContext()->extraFunctions()->glDeleteVertexArrays(1, &vao_);
+    vao_ = 0;
+  }
+
+  if (shaderProgram_ != 0)
+  {
+    glDeleteProgram(shaderProgram_);
+    shaderProgram_ = 0;
+  }
+}
+
+GLuint OpenGLSliceViewer::compileShader(GLenum shaderType, const char* source)
+{
+  const GLuint shader = glCreateShader(shaderType);
+  glShaderSource(shader, 1, &source, nullptr);
+  glCompileShader(shader);
+
+  GLint compileStatus = GL_FALSE;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compileStatus);
+  if (compileStatus != GL_TRUE)
+  {
+    glDeleteShader(shader);
+    return 0;
+  }
+
+  return shader;
 }
 
 void OpenGLSliceViewer::uploadTextureIfNeeded()
