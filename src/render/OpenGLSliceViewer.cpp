c544b8e465446610e596228aa390d3e3a01cb816
William <33472403+wpop@users.noreply.github.com>
2026-06-27 07:47:25 -0700

    Add OpenGL slice viewer skeleton (#62)

---
+++ src/render/OpenGLSliceViewer.cpp
@@ -0,0 +1,28 @@
+#include "qtviewerpro/render/OpenGLSliceViewer.h"
+
+namespace qvp
+{
+
+OpenGLSliceViewer::OpenGLSliceViewer(QWidget* parent) : QOpenGLWidget(parent)
+{
+}
+
+OpenGLSliceViewer::~OpenGLSliceViewer() = default;
+
+void OpenGLSliceViewer::initializeGL()
+{
+  initializeOpenGLFunctions();
+  glClearColor(17.0F / 255.0F, 17.0F / 255.0F, 17.0F / 255.0F, 1.0F);
+}
+
+void OpenGLSliceViewer::resizeGL(int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void OpenGLSliceViewer::paintGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+}
+
+} // namespace qvp
