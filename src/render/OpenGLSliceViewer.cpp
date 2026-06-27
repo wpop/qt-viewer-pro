085b9b0404d511c9b61871eb9a2a2dbaf56d431e
William <33472403+wpop@users.noreply.github.com>
2026-06-27 12:43:59 -0700

    Add OpenGL texture upload support (#65)

--- src/render/OpenGLSliceViewer.cpp
+++ src/render/OpenGLSliceViewer.cpp
@@ -7,11 +7,21 @@ OpenGLSliceViewer::OpenGLSliceViewer(QWidget* parent) : QOpenGLWidget(parent)
 {
 }
 
-OpenGLSliceViewer::~OpenGLSliceViewer() = default;
+OpenGLSliceViewer::~OpenGLSliceViewer()
+{
+  if (textureId_ != 0 && context())
+  {
+    makeCurrent();
+    glDeleteTextures(1, &textureId_);
+    textureId_ = 0;
+    doneCurrent();
+  }
+}
 
 void OpenGLSliceViewer::setImage(const QImage& image)
 {
   image_ = image;
+  textureDirty_ = true;
   update();
 }
 
@@ -23,6 +33,7 @@ bool OpenGLSliceViewer::hasImage() const
 void OpenGLSliceViewer::initializeGL()
 {
   initializeOpenGLFunctions();
+  glGenTextures(1, &textureId_);
   glClearColor(17.0F / 255.0F, 17.0F / 255.0F, 17.0F / 255.0F, 1.0F);
 }
 
@@ -34,6 +45,47 @@ void OpenGLSliceViewer::resizeGL(int width, int height)
 void OpenGLSliceViewer::paintGL()
 {
   glClear(GL_COLOR_BUFFER_BIT);
+  uploadTextureIfNeeded();
+}
+
+void OpenGLSliceViewer::uploadTextureIfNeeded()
+{
+  if (!textureDirty_)
+  {
+    return;
+  }
+
+  if (image_.isNull())
+  {
+    textureDirty_ = false;
+    return;
+  }
+
+  if (textureId_ == 0)
+  {
+    return;
+  }
+
+  glBindTexture(GL_TEXTURE_2D, textureId_);
+
+  const QImage textureImage = image_.convertToFormat(QImage::Format_RGBA8888);
+  glTexImage2D(GL_TEXTURE_2D,
+               0,
+               GL_RGBA,
+               textureImage.width(),
+               textureImage.height(),
+               0,
+               GL_RGBA,
+               GL_UNSIGNED_BYTE,
+               textureImage.constBits());
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  glBindTexture(GL_TEXTURE_2D, 0);
+  textureDirty_ = false;
 }
 
 } // namespace qvp
